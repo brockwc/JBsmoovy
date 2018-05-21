8118d79d93a2b38644b4d551d5b81d5e49ef980c
zap-rowsdower <brock.whitbread@gmail.com>
2018-05-21 12:06:39 -0700

    remove all methods from ingredients controller, pare down methods in the drink controller to return id and title in index and id and description in show

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,16 +1,14 @@
-class DrinksController < ApplicationController
-  before_action :set_drink, only: [:show, :update, :destroy]
-
+class DrinksController < ApiController
   # GET /drinks
   def index
-    @drinks = Drink.all
-
-    render json: @drinks
+    @drinks = Drink.select("id, title").all
+    render json: @drinks.to_json
   end
 
   # GET /drinks/1
   def show
-    render json: @drink
+    @drink = Drink.find(params[:id])
+    render json: @drink.to_json(:include => { :ingredients => { :only => [:id, :description] }})
   end
 
   # POST /drinks
